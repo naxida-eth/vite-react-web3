845fc3534f574c8cc376b833978c6c1ee32adaec
wuming <3261937664@qq.com>
2023-09-12 15:37:27 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,36 @@ import svgr from "vite-plugin-svgr";
 // import nodeBuiltins from "rollup-plugin-node-builtins";
 import commonjsPlugin from "vite-plugin-commonjs";
 // import nodePolyfills from "rollup-plugin-node-polyfills";
-import { Buffer } from "buffer";
+// import { Buffer } from 'Buffer';
 // import vitePluginRequire from "vite-plugin-require";
 // import { viteExternalsPlugin } from "vite-plugin-externals";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vitejs.dev/config/
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import notifier from "vite-plugin-notifier";
+
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+    outDir: "build", //想要把dist修改成什么名字在这边改
+  },
   plugins: [
     react(),
     VitePluginCssModules.default(),
     // vitePluginRequire.default(),
     svgr(),
-    // nodePolyfills(),
     commonjsPlugin(),
+    notifier(),
     // commonjs(),
     // nodeBuiltins(),
+    // nodePolyfills({
+    //   globals: {
+    //     Buffer: true, // can also be 'build', 'dev', or false
+    //     global: true,
+    //     process: true,
+    //   },
+    // }),
   ],
   define: {
     "process.env": process.env,
@@ -30,6 +45,19 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["buffer-polyfill.js"],
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
   },
   resolve: {
     alias: {
@@ -40,6 +68,7 @@ export default defineConfig({
       "src/images": "/src/images",
       // // 创建一个别名，将 'swiper' 映射到插件的根目录
       swiper: path.resolve(__dirname, "node_modules/swiper"),
+      // buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
       // 添加其他别名配置
     },
   },
